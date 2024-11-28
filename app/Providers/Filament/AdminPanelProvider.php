267640f3583f41b797c28367f5179e29f2f98d6f
assulkhaniproject <assulkhaniproject@gmail.com>
2024-11-28 15:06:18 +0700

    admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -87,7 +87,6 @@ class AdminPanelProvider extends PanelProvider
                 FilamentLoggerPlugin::make(),
                 FilamentApexChartsPlugin::make(),
                 LightSwitchPlugin::make()->position(Alignment::TopCenter),
-                // SpotlightPlugin::make(),
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
