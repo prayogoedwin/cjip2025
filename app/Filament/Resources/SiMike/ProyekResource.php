b62aceab58fb608102a5b348c15add83d3a400db
assulkhaniproject <assulkhaniproject@gmail.com>
2025-02-06 22:00:12 +0700

    table proyek simike

--- app/Filament/Resources/SiMike/ProyekResource.php
+++ app/Filament/Resources/SiMike/ProyekResource.php
@@ -550,28 +550,28 @@ class ProyekResource extends Resource
                             return true;
                         }),
 
-                    SelectFilter::make('kecamatan_usaha')
-                        ->label('Kecamatan Usaha')
-                        ->searchable()
-                        ->multiple()
-                        ->default(function () {
-                            if (auth()->user()->kabkota->id) {
-                                return true;
-                            }
-                            return false;
-                        })
-                        ->options(function () {
-                            $kec_usahas = Proyek::where('kab_kota_id', auth()->user()->kabkota->id)
-                                ->pluck('kecamatan_usaha')->toArray();
-                            $kec_usaha = array_combine($kec_usahas, $kec_usahas);
-                            return $kec_usaha;
-                        })
-                        ->visible(function () {
-                            if (auth()->user()->hasRole('kabkota')) {
-                                return true;
-                            }
-                            return false;
-                        }),
+                    // SelectFilter::make('kecamatan_usaha')
+                    //     ->label('Kecamatan Usaha')
+                    //     ->searchable()
+                    //     ->multiple()
+                    //     ->default(function () {
+                    //         if (auth()->user()->kabkota->id) {
+                    //             return true;
+                    //         }
+                    //         return false;
+                    //     })
+                    //     ->options(function () {
+                    //         $kec_usahas = Proyek::where('kab_kota_id', auth()->user()->kabkota->id)
+                    //             ->pluck('kecamatan_usaha')->toArray();
+                    //         $kec_usaha = array_combine($kec_usahas, $kec_usahas);
+                    //         return $kec_usaha;
+                    //     })
+                    //     ->visible(function () {
+                    //         if (auth()->user()->hasRole('kabkota')) {
+                    //             return true;
+                    //         }
+                    //         return false;
+                    //     }),
 
                     SelectFilter::make('kbli')
                         ->label('KBLI')
