0a3109ebaaee3552ffe19ec1f3f463e65174a82a
assulkhaniproject <assulkhaniproject@gmail.com>
2025-02-18 09:01:03 +0700

    orderby produk kemitraan

--- app/Livewire/Frontend/Kemitraan/Produk/ProductList.php
+++ app/Livewire/Frontend/Kemitraan/Produk/ProductList.php
@@ -13,20 +13,20 @@ class ProductList extends Component
     public function cariProduct()
     {
         $this->products = Product::where('name', 'like', '%' . $this->search . '%')
-            ->where('is_active', 1)->paginate(12);
+            ->where('is_active', 1)->orderBy('created_at', 'desc')->paginate(12);
     }
     public function mount()
     {
 
-        $this->products = Product::where('is_active', 1)->paginate(12);
+        $this->products = Product::where('is_active', 1)->orderBy('created_at', 'desc')->paginate(12);
     }
     public function render()
     {
         if (!$this->products) {
-            $this->products = Product::where('is_active', 1)->paginate(12);
+            $this->products = Product::where('is_active', 1)->orderBy('created_at', 'desc')->paginate(12);
         }
 
         $products = $this->products;
-        return view('livewire.frontend.kemitraan.produk.product-list',compact('products'))->layout('components.layouts.master');
+        return view('livewire.frontend.kemitraan.produk.product-list', compact('products'))->layout('components.layouts.master');
     }
 }
