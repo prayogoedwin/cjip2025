954809ac2230f382cc1cafe85bc9455b52fa2526
assulkhaniproject <assulkhaniproject@gmail.com>
2025-02-24 14:23:07 +0700

    multi language create berita

--- app/Filament/Clusters/Cjip/Resources/BeritaResource/Pages/CreateBerita.php
+++ app/Filament/Clusters/Cjip/Resources/BeritaResource/Pages/CreateBerita.php
@@ -4,9 +4,17 @@ namespace App\Filament\Clusters\Cjip\Resources\BeritaResource\Pages;
 
 use App\Filament\Clusters\Cjip\Resources\BeritaResource;
 use Filament\Actions;
+use Filament\Actions\LocaleSwitcher;
 use Filament\Resources\Pages\CreateRecord;
 
 class CreateBerita extends CreateRecord
 {
+    use CreateRecord\Concerns\Translatable;
+    protected function getHeaderActions(): array
+    {
+        return [
+            LocaleSwitcher::make(),
+        ];
+    }
     protected static string $resource = BeritaResource::class;
 }
