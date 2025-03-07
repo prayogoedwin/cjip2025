cfec4d3f1b3209d5cd0d441486929fbf87783556
assulkhaniproject <assulkhaniproject@gmail.com>
2025-03-08 03:24:37 +0700

    permission peta proyek

--- app/Filament/Pages/MapsProyekInvestasi.php
+++ app/Filament/Pages/MapsProyekInvestasi.php
@@ -3,10 +3,13 @@
 namespace App\Filament\Pages;
 
 use App\Filament\Clusters\Cjip;
+use BezhanSalleh\FilamentShield\Traits\HasPageShield;
 use Filament\Pages\Page;
 
 class MapsProyekInvestasi extends Page
 {
+
+    use HasPageShield;
     protected static ?string $navigationIcon = 'heroicon-o-map-pin';
     protected static ?string $navigationLabel = 'Peta Proyek Investasi';
     protected static ?string $title = 'Peta Proyek Investasi';
