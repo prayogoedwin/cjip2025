46c0dbd63d27f01e6c2306fcb1824bef7c49a9d9
assulkhaniproject <assulkhaniproject@gmail.com>
2025-03-11 13:59:53 +0700

    optimize chart

--- app/Filament/Widgets/TopSkalaUsaha.php
+++ app/Filament/Widgets/TopSkalaUsaha.php
@@ -21,8 +21,7 @@ class TopSkalaUsaha extends ApexChartWidget
         $kecamatan_usaha,
         $tanggal_terbit_oss,
         $tanggal, $start, $end;
-    // protected static ?string $loadingIndicator = 'Loading...';
-    // protected static bool $deferLoading = true;
+
     protected $listeners = ['filterUpdated' => 'updateFilter'];
     protected static ?string $loadingIndicator = 'Loading...';
     protected static bool $deferLoading = true;
@@ -44,37 +43,20 @@ class TopSkalaUsaha extends ApexChartWidget
         $this->uraian_skala_usaha = $uraian_skala_usaha['uraian_skala_usaha'];
     }
 
-    /**
-     * Chart Id
-     *
-     * @var string
-     */
     protected static ?string $chartId = 'TopSkalaUsaha';
-
-    /**
-     * Widget Title
-     *
-     * @var string|null
-     */
     protected static ?string $heading = 'Jumlah Proyek Per Skala Usaha';
 
-    /**
-     * Chart options (series, labels, types, size, animations...)
-     * https://apexcharts.com/docs/options
-     *
-     * @return array
-     */
     protected function getOptions(): array
     {
-        $tahun = $this->filterFormData['tahun'] ?? null;
-        $tanggalTerbitOSS = $this->filterFormData['tanggal_terbit_oss'] ?? null;
+        $tahun = $this->filterFormData['tahun'] ?? $this->tahun;
+        $tanggalTerbitOSS = $this->filterFormData['tanggal_terbit_oss'] ?? $this->tanggal_terbit_oss;
         $kabkota = $this->filterFormData['kabkota'] ?? null;
         $sektor = $this->filterFormData['sektor'] ?? null;
         $uraianSkalaUsaha = $this->filterFormData['uraian_skala_usaha'] ?? null;
 
         if (is_array($tanggalTerbitOSS) && isset($tanggalTerbitOSS['start'], $tanggalTerbitOSS['end'])) {
-            $startDate = \Carbon\Carbon::parse($tanggalTerbitOSS['start'])->format('Y-m-d');
-            $endDate = \Carbon\Carbon::parse($tanggalTerbitOSS['end'])->format('Y-m-d');
+            $startDate = Carbon::parse($tanggalTerbitOSS['start'])->format('Y-m-d');
+            $endDate = Carbon::parse($tanggalTerbitOSS['end'])->format('Y-m-d');
         } else {
             $startDate = $endDate = null;
         }
@@ -88,27 +70,14 @@ class TopSkalaUsaha extends ApexChartWidget
             $this->uraian_skala_usaha,
             $this->kecamatan_usaha
         )
-            ->select(
-                'uraian_skala_usaha',
-                DB::raw('count(CASE WHEN dikecualikan = "1" OR is_mapping = "0" THEN nib_count ELSE 0 END) as `project_count`')
-            )
+            ->select('uraian_skala_usaha')
+            ->selectRaw('COUNT(CASE WHEN dikecualikan = "1" OR is_mapping = "0" THEN nib_count ELSE 0 END) as `project_count`')
             ->groupBy('uraian_skala_usaha')
             ->orderByDesc(DB::raw('COUNT(*)'))
             ->whereIn('uraian_skala_usaha', ['Usaha Kecil', 'Usaha Mikro'])
-            // ->when($tahun, function ($query, $tahun) {
-            //     return $query->where('tahun', $tahun);
-            // })
-            // ->when($kabkota, function ($query, $kabkota) {
-            //     return $query->where('kab_kota_id', $kabkota);
-            // })
-            // ->when($sektor, function ($query, $sektor) {
-            //     return $query->where('sektor_id', $sektor);
-            // })
-            // ->when($startDate && $endDate, function ($query) use ($startDate, $endDate) {
-            //     return $query->whereBetween('tanggal_terbit_oss', [$startDate, $endDate]);
-            // })
             ->get();
 
+        // Extract data for chart
         $uraianSkalaUsaha = $proyekData->pluck('uraian_skala_usaha')->toArray();
         $projectCount = $proyekData->pluck('project_count')->toArray();
 
@@ -119,7 +88,7 @@ class TopSkalaUsaha extends ApexChartWidget
             ],
             'series' => $projectCount,
             'labels' => $uraianSkalaUsaha,
-            'colors' => ['#16a34a', '#f59e0b', '#9333ea', '#e11d48', '#3b82f6'],
+            'colors' => ['#16a34a', '#f59e0b'],
         ];
     }
 }
