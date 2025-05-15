532688a613d8c6016f1621aa7bc2bc398c9caaa2
assulkhaniproject <assulkhaniproject@gmail.com>
2025-05-15 09:28:54 +0700

    permission banner

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -87,6 +87,7 @@ class AdminPanelProvider extends PanelProvider
                 BannerPlugin::make()
                     ->persistsBannersInDatabase()
                     ->navigationGroup('Super Admin')
+                    ->bannerManagerAccessPermission('banner-manager')
                     ->navigationIcon('')->navigationSort(10),
                 SpotlightPlugin::make(),
                 // FilamentProgressbarPlugin::make()->color('#16a34a'),
